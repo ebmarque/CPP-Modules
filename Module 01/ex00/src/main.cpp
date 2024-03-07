399e5a1f8ae76bc99aec5c97cf21b4c7b014b41f
ebmarque <ebenezermarquezine@gmail.com>
2024-03-07 11:13:43 +0000

    Solution to exercise 00

---
+++ Module 01/ex00/src/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebmarque <ebmarque@student.42porto.com     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 08:52:58 by ebmarque          #+#    #+#             */
+/*   Updated: 2024/03/07 10:22:11 by ebmarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Zombie.hpp"
+
+Zombie* newZombie(std::string name)
+{
+	Zombie *fresh_zombie = new Zombie(name);
+	return (fresh_zombie);
+}
+
+void randomChump(std::string name)
+{
+	Zombie *random = newZombie(name);
+	random->announce();
+	delete random;
+}
+
+int main(void)
+{
+	randomChump("Foo");
+	
+	return (0);
+}
+
+
