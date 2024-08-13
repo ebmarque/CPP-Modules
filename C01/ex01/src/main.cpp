762138895ad79d6d05577a45a3113f6849388d95
ebmarque <ebenezermarquezine@gmail.com>
2024-08-13 14:50:31 +0100

    C01 ex01 allocation size protection corrected

--- C01/ex01/src/main.cpp
+++ C01/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ebmarque <ebmarque@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 08:52:58 by ebmarque          #+#    #+#             */
-/*   Updated: 2024/03/07 11:11:20 by ebmarque         ###   ########.fr       */
+/*   Updated: 2024/08/13 14:50:04 by ebmarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ void randomChump(std::string name)
 
 Zombie* zombieHorde(int N, std::string name)
 {
+	if (N <= 0)
+		return (NULL);
 	Zombie *horde = new Zombie[N];
 	for (int i = 0; i < N; i++)
 	{
@@ -38,7 +40,7 @@ Zombie* zombieHorde(int N, std::string name)
 
 int main(void)
 {
-	Zombie *horde = zombieHorde(7, "Foo");
+	Zombie *horde = zombieHorde(-1, "Foo");
 	delete []horde;
 	return (0);
 }
